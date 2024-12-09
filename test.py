150f2a174ea39fff5e728ac51714626331f2dd72
Artemii Olefirenko <olephart05@gmail.com>
2024-12-09 21:09:56 +0100

    commit

--- test.py
+++ test.py
@@ -1,21 +1,57 @@
-import whisper
-import ssl
+import os
+import tempfile
 from gpt4all import GPT4All
+from whisper import load_model
+from flask import Flask, jsonify, request
 
-ssl._create_default_https_context = ssl.create_default_context()
-try:
-    model = whisper.load_model("base")
-    result = model.transcribe("733ec6f5-f063-4df5-9e08-4687bf2cbe56.mp4")
-    print(f' The text in video: \n {result["text"]}')
+app = Flask(__name__)
 
-    data = result["text"]
-    prompt = f'''Provide me point wise summary of text "{data}".'''
+model = load_model("base")
 
-    model = GPT4All("Llama-3.2-3B-Instruct-Q4_0.gguf", allow_download=True)
+# Allowed MIME types for video/audio files
+ALLOWED_MIME_TYPES = {"video/mp4", "audio/mpeg", "video/quicktime"}  # mp4, mp3, mov
 
-    output = model.generate(prompt)
+@app.route("/upload-video/", methods = ['POST'])
+def upload_video():
+    if 'video' not in request.files:
+        return jsonify({"error": "No video part in the request"}), 400
+    # Extract video file from request
+    file = request.files['video']
+    if file.filename == '':
+        return jsonify({"error": "No file selected"}), 400
+    if file and file.mimetype in ALLOWED_MIME_TYPES :
+        # Save the file temporarily for processing
+        with tempfile.NamedTemporaryFile(delete=False) as temp_video:
+            file.save(temp_video.name)
+            temp_video_path = temp_video.name
 
-    print(f' The summary of meeting : \n {output}')
-except Exception as e:
-    print(f'Error: {e}')
+        try:
+            # Transcribe the file using Whisper
+            transcription = transcribe_file(temp_video_path)
+            # Get the summary of the text through GPT4All
+            result = get_summary(transcription)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+        finally:
+            # Clean up temporary file
+            os.remove(temp_video_path)
 
+        return result, 200
+    else:
+        return jsonify({"error": "Invalid file type. Allowed types: {', '.join(ALLOWED_MIME_TYPES)}"}), 400
+
+
+def transcribe_file(file_path):
+    # Load the model and transcribe the video
+    result = model.transcribe(file_path)
+    return result['text']
+
+def get_summary(text):
+    print(text)
+    prompt = "Provide a brief summary of the text: " + text + ". Format should be: you begin with here is the summary (on the first line): and then below just write your brief but informative summary of the given text."
+    model_gpt = GPT4All("Llama-3.2-3B-Instruct-Q4_0.gguf", allow_download=True)
+    output = model_gpt.generate(prompt)
+    return output
+
+if __name__ == '__main__':
+    app.run(debug=True)
